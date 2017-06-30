5700bdbeb6a5fbbaa3f7c11ca027788e625905c8
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-06-30 16:26:14 -0300

    bad fluentD commit

--- FluentD/src/main/java/com/github/nicolasperuch/FluentDSample.java
+++ FluentD/src/main/java/com/github/nicolasperuch/FluentDSample.java
@@ -1,6 +1,22 @@
 package com.github.nicolasperuch;
 
+import org.fluentd.logger.FluentLogger;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FluentDSample {
 
+    private static FluentLogger LOG = FluentLogger.getLogger("fluentd.test");
+
+    public static void main(String[] args) {
+
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("from", "userA");
+        data.put("to", "userB");
+        LOG.log("follow", data);
+
+
+    }
 
 }
