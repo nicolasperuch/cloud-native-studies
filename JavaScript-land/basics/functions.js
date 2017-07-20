aab95d73be1caaee0a6ca5374686d867cc7029f9
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-07-20 17:25:43 -0300

    Adjusting function name

--- JavaScript-land/basics/functions.js
+++ JavaScript-land/basics/functions.js
@@ -1,5 +1,5 @@
 var sum = function (number1, number2) {
-    return function redundat (){
+    return function redundatSumFunction(){
         return number1 + number2;
     }
 }
@@ -7,9 +7,7 @@ var sum = function (number1, number2) {
 console.log(sum(2, 4)());
 
 console.log(anotherWayToSum(3, 1));
-
-// quando nao atribuimos a funcao nós podemos fazer hoisting
-
+// quando nao atribuimos a funcao a alguma variavel nós podemos fazer hoisting
 function anotherWayToSum(number1, number2){
     return number1 + number2;
 }
