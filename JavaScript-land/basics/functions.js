bd21f59ddd0389f65eee234eb63ba41a32d2ff10
Nicolas Peruch <nicolasperuch.2010@gmail.com>
2017-07-20 18:04:02 -0300

    Adding call-and-apply example

--- JavaScript-land/basics/functions.js
+++ JavaScript-land/basics/functions.js
@@ -3,8 +3,8 @@ var sum = function (number1, number2) {
         return number1 + number2;
     }
 }
-
 console.log(sum(2, 4)());
+
 
 console.log(anotherWayToSum(3, 1));
 // quando nao atribuimos a funcao a alguma variavel nós podemos fazer hoisting
@@ -12,3 +12,23 @@ function anotherWayToSum(number1, number2){
     return number1 + number2;
 }
 
+
+var product = {
+    type: "t-shirt",
+    name: "Adidas t-shirt",
+    cost: 99.99
+}
+
+var calculateLowerTaxesFromProduct = function (product){
+    return product.cost * 0.2; 
+}
+var calculateHighTaxesFromProduct = function (product){
+    return product.cost * 0.25; 
+}
+var addTaxesToProduct = function (product, calculateTaxes){
+    return product.cost + calculateTaxes(product)
+}
+
+console.log(product)
+console.log(calculateHighTaxesFromProduct(product))
+console.log(addTaxesToProduct(product, calculateHighTaxesFromProduct))
